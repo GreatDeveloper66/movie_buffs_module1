2d369cb948577942c7a4d52d7ec21d8bf06fbb80
cooljasonmelton <cooljasonmelton>
2020-03-03 10:55:48 -0600

    enter username method

--- bin/run.rb
+++ bin/run.rb
@@ -18,14 +18,23 @@ require_relative '../config/environment'
 
 
 
-#menu
+def enter_username
+  puts "Welcome, enter your username:"
+  username = gets.chomp
+  if User.find_by(name: username)
+    puts "Hello #{username}"
+  else 
+    puts "#{username} is not in our system. Create a new username."
+    create_username
+  end 
+end 
+
 def menu 
   puts "Welcome, here are your options:"
-  puts "1 Create Username"
-  puts "2 Watch a Movie"
-  puts "3 Update Username"
-  puts "4 See Movie List"
-  puts "5 Delete a Movie Viewing"
+  puts "1 Watch a Movie"
+  puts "2 Update Username"
+  puts "3 See Movie List"
+  puts "4 Delete a Movie Viewing"
 end 
 
 def get_input
@@ -38,10 +47,25 @@ def create_username
   input_name = gets.chomp
   User.create(name: input_name)
 end 
-menu
-get_input
-create_username
-#create username
+
+def create_view
+  # puts "What is your username?"
+  # input_user = gets.chomp 
+  # find_user = User.find_by(name: input_user.to_s)
+  # if find_user 
+  #    this_user_id = find_user.id 
+  # else 
+  #   new_user = create_username
+  #   this_user_id = new_user.id
+  # end
+  # puts "What movie did you watch?"
+  # Movie.id
+  # puts "Enter how long you watched the movie:"
+  # duration = gets.chomp
+
+end 
+
+enter_username
 
 #create view
 
