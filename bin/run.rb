ba65eb3b8dba67e0d722b0828f694ac6615693bf
cooljasonmelton <cooljasonmelton>
2020-03-05 11:55:19 -0600

    added feature play next in queue

--- bin/run.rb
+++ bin/run.rb
@@ -7,19 +7,6 @@ def enter_username
   new_user = User.find_or_create_by(name: username)
 end 
 
-def menu 
-  [
-   "1 Add Movie to Queue",
-   "2 Watch a Movie from Your Queue",
-   "3 See All Movie Available",
-   "4 See All Movies In Queue",
-   "5 See All Users Who Viewed A Movie",
-   "6 Update Username",
-   "7 Delete a User",
-   "8 Quit"
-  ]
-end 
-
 def get_input
   puts "Select an option using a number."
   gets.chomp
@@ -62,11 +49,24 @@ def pick_movie_from_queue(user)
   end 
 end 
 
+def menu 
+  [
+   "1 Add Movie to Queue",
+   "2 Play the Next Movie in Your Queue",
+   "3 Select a Movie from Your Queue",
+   "4 See All Movies Available",
+   "5 See All Movies In Queue",
+   "6 See All Users Who Viewed A Movie",
+   "7 Update Username",
+   "8 Delete a User",
+   "9 Quit"
+  ]
+end 
 
 def run_movie_buffs
   the_current_user = enter_username
   input = "string"
-  until input.chr == "8" do
+  until input.chr == "9" do
     menu
     input = TTY::Prompt.new.select("Select an option", menu)
     case input.chr
@@ -74,20 +74,22 @@ def run_movie_buffs
         the_current_user.reload
         the_current_user.create_view
       when "2"
+        the_current_user.watch_next_movie_in_queue
+      when "3"
         pick_movie_from_queue(the_current_user)
-      when "3" 
+      when "4" 
         see_movie_list
-      when "4"
+      when "5"
         the_current_user.reload
         puts the_current_user.show_users_queue_titles
-      when "5" 
+      when "6" 
         movie_views
-      when "6"
-        update_username(the_current_user)
       when "7"
-        the_current_user.destroy
-        input = "8"
+        update_username(the_current_user)
       when "8"
+        the_current_user.destroy
+        input = "9"
+      when "9"
         puts "Goodbye"
       else 
         puts "Invalid input"
--- lib/user.rb
+++ lib/user.rb
@@ -3,36 +3,32 @@ class User < ActiveRecord::Base
   has_many :movies, through: :views
 
 
+  # this creates a new view connecting a user id and movie id.
+  # watched defaults to false adding this view to a user's queue (watched: false)
   def create_view
     movie = Movie.find_by(title: get_movie)
     new_view = View.create(user_id: self.id, movie_id: movie.id)
-    # binding.pry
-    # User.views << new_view
   end 
 
-  # def show_users_movies
-  #   # binding.pry
-  #   self.movies.each do |v|
-  #     puts v.title
-  #   end 
-  # end 
 
+  # this filters the views showing only showing the views of this user
   def show_users_queue
     self.views.select do |v|
       !v.watched
     end 
   end 
 
+  def watch_next_movie_in_queue
+    user_queue = self.show_users_queue
+    user_queue.first.update(watched: true)
+  end 
+
+  # this maps show_users_queue to give a list of just the titles in this users queue
+  # this method is used for TTY prompts
   def show_users_queue_titles
     self.show_users_queue.map do |v|
       v.movie.title
     end
-  end 
-
-  def show_users_watched
-    self.views.select do |v|
-      v.watched
-    end 
   end 
 
   def find_view_by_movie_id(mov_id)
@@ -45,32 +41,12 @@ class User < ActiveRecord::Base
   end 
 
 
-
-
-  # def show_watched_movies
-  #   self.views.select do |v|
-  #     v.watched 
-  #   end
-
-  # end
-
-  # def show_unwatched_movies
-  #   self.views.select do |v|
-  #     !v.watched 
-  #   end
-  # end
-
-  # def show_unwatched_movie_titles
-  #   self.show_unwatched_movies.map do |m|
-  #     m.title
-  #   end 
-  # end 
-
-  # def show_watched_movie_titles
-  #   self.show_watched_movies.map do |m|
-  #     m.title
-  #   end 
-  # end 
-
+   #this filters a users views to show only those that have been watched
+   # we haven't used this but could be useful for a new feature "watched list"
+   def show_users_watched
+    self.views.select do |v|
+      v.watched
+    end 
+  end 
 
 end
